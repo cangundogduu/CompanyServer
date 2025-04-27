8f021baa204f5a638a9ec3ebe454c2b200cecd06
can gündoğdu <gundogdu.can@hotmail.com>
2025-04-27 22:58:11 +0300

    Add project files.

---
+++ Company.API/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Company.API.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual IList<Product> Products { get; set; }
+    }
+}
